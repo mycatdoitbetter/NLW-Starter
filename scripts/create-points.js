856276e0fdc50866a694e14b80093dedf5ad3ebf
mycatdoitbetter <andrecasteloecomp@gmail.com>
2020-06-07 23:35:29 -0300

    Adding modal search and result list

--- scripts/create-points.js
+++ scripts/create-points.js
@@ -1,7 +1,4 @@
-// document.querySelector("select[name=uf]").addEventListener("change", () => {
-//   console.log("MUDEI!");
-// });
-
+// fill the UF select of form
 function populateUFs() {
   const ufSelect = document.querySelector("select[name=uf]");
 
@@ -15,6 +12,7 @@ function populateUFs() {
 }
 populateUFs();
 
+// fill the city select of form
 function getCities(event) {
   const citySelect = document.querySelector("select[name=city]");
   const stateInput = document.querySelector("input[name=state]");
@@ -24,11 +22,14 @@ function getCities(event) {
   const indexOfSelectedState = event.target.selectedIndex;
   stateInput.value = event.target.options[indexOfSelectedState].text;
 
+  citySelect.innetHTML = `<option value="">Selecione uma cidade</option>`;
+  citySelect.disabled = true;
   fetch(
     `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUfId}/municipios`
   )
     .then((res) => res.json())
     .then((cities) => {
+      citySelect.innerHTML = "";
       for (const city of cities) {
         citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
       }
@@ -37,3 +38,30 @@ function getCities(event) {
     });
 }
 document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+// Select the items grid
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+let selectedItems = [];
+const collectedItems = document.querySelector("input[name=items]");
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  const alreadySelected = selectedItems.find((item) => item === itemId);
+
+  if (alreadySelected) {
+    selectedItems.splice(selectedItems.indexOf(itemId), 1);
+  } else {
+    selectedItems.push(itemId);
+  }
+
+  collectedItems.value = selectedItems;
+}
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
